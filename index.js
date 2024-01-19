76ac460673d50f2293c52b945a8ddfbc8f3b76f4
ray <ray840106@gmail.com>
2024-01-19 16:49:46 +0800

    v2.0.0

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 // 引入 library
 require('dotenv').config()
-const express = require('express');
-const session = require("express-session");
-const bodyParser = require('body-parser');
-const requestIp = require('request-ip');
-const path = require("path");
-const jwt = require("jsonwebtoken");
-const SocketServer = require('ws').Server
-const fileUpload = require('express-fileupload');
 const cors = require('cors')
+const path = require('path')
+const debug = require('debug')
+const log4js = require("log4js");
+const express = require('express');
+const jwt = require("jsonwebtoken");
+const requestIp = require('request-ip');
+const bodyParser = require('body-parser');
+const session = require("express-session");
+const fileUpload = require('express-fileupload');
+const logConfig = require("./config/log4js.json");
 
 const app = express();
 const port = process.env.PORT || '5001'
 
+const SocketServer = require('ws').Server
 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 let router = require('./router');
+
+/**make a log directory, just in case it isn't there.**/
+try {
+    require("fs").mkdirSync("./logs");
+} catch (e) {
+    if (e.code != "EEXIST") {
+        console.error("Could not set up log directory, error was: ", e);
+        process.exit(1);
+    }
+}
+
+/**
+ * Initialise log4js first, so we don't miss any log messages
+ */
+log4js.configure(logConfig);
+const log = log4js.getLogger("connection");
+const logger = log4js.getLogger();
+
+console.log = createLogProxy("debug", logger);
+console.error = createLogProxy("error", logger);
 
 /******************************************************************
  ********************* session設定 *********************************
@@ -31,7 +56,9 @@ app.use(
 /******************************************************************
  ******* 訪問靜態文件，這裡是訪問所有dist目錄下的靜態資源文件 **************
  ******************************************************************/
-app.use(express.static(path.resolve(__dirname, './dist')))
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+// app.use(express.static(path.resolve(__dirname, './dist')))
 app.use(express.static('public')); //to access the files in public folder
 app.use(cors()); // it enables all cors requests
 app.use(fileUpload());
@@ -39,7 +66,6 @@ app.use(requestIp.mw({attributeName: 'clientIP'}))
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/API/',router)
-
 
 const server = app.listen(port, () => {
     console.log(`Listening on ${port}`)
@@ -83,3 +109,60 @@ wss.on('connection', ws => {
     })
 
 })
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function createLogProxy(logLevel, logger) {
+    return (...param) => {
+        logger[logLevel](...param);
+    };
+}
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+    log.info(
+        "Express server listening on port ",
+        server.address().port,
+        " with pid ",
+        process.pid
+    );
+}
--- router.js
+++ router.js
@@ -1,11 +1,49 @@
-let express = require('express')
-let router = express.Router()
-let auth_api = require('./api/auth')
-let friends_api = require('./api/friends')
-let rooms_api = require('./api/rooms')
-let messages_api = require('./api/messages')
-let file_api = require('./api/file')
+const express = require('express')
+const router = express.Router()
+const conn = require("./models/db_conn");
+let file_api = require('./controllers/file')
+let auth_api = require('./controllers/auth')
+let rooms_api = require('./controllers/rooms')
+let payment_api = require('./controllers/payment')
+let article_api = require('./controllers/article')
+let friends_api = require('./controllers/friends')
+let messages_api = require('./controllers/messages')
+let stickers_api = require('./controllers/stickers')
+let table_own_sticker = require("./models/table_own_sticker");
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const {HmacSHA256} = require("crypto-js");
+const Base64 = require("crypto-js/enc-base64");
+const ecpay_payment = require("ecpay_aio_nodejs");
+const {LINE_PAY_SECRET,LINE_PAY_VERSION,LINE_PAY_CHANNELID,LINE_PAY_SITE,ECPAY_MERCHANTID, ECPAY_HASHKEY, ECPAY_HASHIV, HOST, SECRET}=process.env;
 
+const options = {
+    OperationMode: 'Test', //Test or Production
+    MercProfile: {
+        MerchantID: ECPAY_MERCHANTID,
+        HashKey: ECPAY_HASHKEY,
+        HashIV: ECPAY_HASHIV,
+    },
+    IgnorePayment: [
+        //    "Credit",
+        //    "WebATM",
+        //    "ATM",
+        //    "CVS",
+        //    "BARCODE",
+        //    "AndroidPay"
+    ],
+    IsProjectContractor: false,
+};
+const MerchantTradeDate = new Date().toLocaleString('zh-TW', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+    timeZone: 'UTC',
+});
 /**
     login, register
  */
@@ -31,6 +69,58 @@ router.post('/messages', messages_api.messages)
  */
 router.post('/file', file_api.file)
 
+/**
+ CRUD stickers
+ */
+router.post('/stickers', stickers_api.stickers)
+
+/**
+ CRUD article
+ */
+router.post('/article', article_api.article)
+
+/**
+ CRUD payment
+ */
+router.post('/payment', payment_api.payment)
+router.get('/linePay/confirm',async (req,res)=>{
+    const { transactionId,orderId } = req.query;
+    const connection = await conn.getConnection();
+    try{
+        // 購買貼圖群
+        let upd_own_sticker = await table_own_sticker.updOwnSticker(connection,1,orderId)
+        if (!upd_own_sticker.done_TF) {
+            throw upd_own_sticker;
+        }
+
+        let get_sticker_list = await table_own_sticker.getStickerList(connection,'',orderId)
+        const linePayBody = {
+            amount: get_sticker_list[0].PRICE,
+            currency: 'TWD',
+        }
+        const uri = `/payments/${transactionId}/confirm`;
+        const nonce = orderId;
+        const string = `${LINE_PAY_SECRET}/${LINE_PAY_VERSION}${uri}${JSON.stringify(linePayBody)}${nonce}`
+        const signature = Base64.stringify(HmacSHA256(string,LINE_PAY_SECRET))
+        const headers = {
+            "Content-Type": "application/json",
+            "X-LINE-ChannelId": LINE_PAY_CHANNELID,
+            "X-LINE-Authorization-Nonce": nonce,
+            "X-LINE-Authorization": signature,
+        }
+        const url = `${LINE_PAY_SITE}/${LINE_PAY_VERSION}${uri}`;
+        const linePayRes = await axios.post(url, linePayBody, { headers });
+        res.redirect(`http://localhost:3000/#/stickerStore?orderId=${orderId}&done=${linePayRes?.data?.returnCode === '0000'}`)
+    }catch (e) {
+        res.status(500).json({
+            status: 'error',
+            code: 500,
+            data: e,
+            message: 'Internal Server Error',
+        });
+    }
+
+})
 
 router.get("/logout",(req,res) => {
     req.session.destroy();
